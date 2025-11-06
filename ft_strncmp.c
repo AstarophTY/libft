6802882532f9e8578a15b1ccb204e066bae2bd83
Swann Gil--Desfrennes <sgil--de@z2r3p8.42lyon.fr>
2025-11-06 19:16:51 +0100

    add strncmp cast to unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sgil--de <sgil--de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 10:24:53 by sgil--de          #+#    #+#             */
-/*   Updated: 2025/11/06 17:48:36 by sgil--de         ###   ########.fr       */
+/*   Updated: 2025/11/06 19:12:53 by sgil--de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	const unsigned char	*s1_cpy;
+	const unsigned char	*s2_cpy;
+	size_t				i;
 
 	i = 0;
+	s1_cpy = (const unsigned char *)s1;
+	s2_cpy = (const unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	while (i < n - 1 && s1[i] && s1[i] == s2[i])
+	while (i < n - 1 && s1_cpy[i] && s1_cpy[i] == s2_cpy[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return (s1_cpy[i] - s2_cpy[i]);
 }
